4715b8f30cd8cb3657454a186e85959c729eca20
jmcunst <xnslqjtmghaf@gmail.com>
2023-08-13 15:55:51 +0900

    feat: front controller & mapping/binding

--- src/main/java/jmcunst/spring001/helloboot/HellobootApplication.java
+++ src/main/java/jmcunst/spring001/helloboot/HellobootApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
@@ -19,16 +20,28 @@ public class HellobootApplication {
     public static void main(String[] args) {
         TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
         WebServer webServer = serverFactory.getWebServer(servletContext -> {
-            servletContext.addServlet("hello", new HttpServlet() {
+            HelloController helloController = new HelloController();
+            servletContext.addServlet("frontcontroller", new HttpServlet() { // 18. 프론트컨트롤러
                 @Override
                 protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-                    String name = req.getParameter("name");
+                    // 인증, 보안, 다국어, 공통 기능
+                    if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())){ // 20. 맵핑
+                        String name = req.getParameter("name");
 
-                    resp.setStatus(HttpStatus.OK.value());
-                    resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-                    resp.getWriter().println("Hello " + name);
+                        String ret = helloController.hello(name); // 20. 바인딩
+
+                        resp.setStatus(HttpStatus.OK.value());
+                        resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                        resp.getWriter().println(ret);
+                    }
+                    else if (req.getRequestURI().equals("/user")) {
+
+                    }
+                    else {
+                        resp.setStatus(HttpStatus.NOT_FOUND.value());
+                    }
                 }
-            }).addMapping("/hello");
+            }).addMapping("/*"); // 18. 프론트컨트롤러
         }); // 웹 서버 생성 // 파라미터(ServletContextInitializer)를 통해 서블릿 등록 // Lambda식으로 servletContext로 사용
         webServer.start(); // 톰켓 서블릿 컨테이너가 동작
     }
