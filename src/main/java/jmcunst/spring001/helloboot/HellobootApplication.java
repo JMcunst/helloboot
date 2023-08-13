6992b942ccb9b6029872409d712e750f362f17f9
jmcunst <xnslqjtmghaf@gmail.com>
2023-08-13 14:52:38 +0900

    feat: register servlet and manage req&resp

--- src/main/java/jmcunst/spring001/helloboot/HellobootApplication.java
+++ src/main/java/jmcunst/spring001/helloboot/HellobootApplication.java
@@ -1,13 +1,36 @@
 package jmcunst.spring001.helloboot;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class HellobootApplication {
 
     public static void main(String[] args) {
-        new TomcatServletWebServerFactory();
+        TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+        WebServer webServer = serverFactory.getWebServer(servletContext -> {
+            servletContext.addServlet("hello", new HttpServlet() {
+                @Override
+                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                    String name = req.getParameter("name");
+
+                    resp.setStatus(HttpStatus.OK.value());
+                    resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                    resp.getWriter().println("Hello " + name);
+                }
+            }).addMapping("/hello");
+        }); // 웹 서버 생성 // 파라미터(ServletContextInitializer)를 통해 서블릿 등록 // Lambda식으로 servletContext로 사용
+        webServer.start(); // 톰켓 서블릿 컨테이너가 동작
     }
 
 }
