4cf80e69c5baa2beeaef64d9aa65eb5e14927e54
jmcunst <xnslqjtmghaf@gmail.com>
2023-08-15 00:00:06 +0900

    feat: spring boot

--- src/main/java/jmcunst/spring001/helloboot/HellobootApplication.java
+++ src/main/java/jmcunst/spring001/helloboot/HellobootApplication.java
@@ -1,12 +1,11 @@
 package jmcunst.spring001.helloboot;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 // 스프링 컨테이너가 팩토리 메소드를 가진 클래스다라는 것을 인지하도록 함
@@ -25,23 +24,9 @@ public class HellobootApplication {
 
     public static void main(String[] args) {
         // 28. GenericWebApplicationContext은 자바 코드로 만든 환경설정 정보를 읽을 수 없다.
-        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
-            @Override
-            protected void onRefresh() {
-                super.onRefresh();
-                ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
-                DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
-                dispatcherServlet.setApplicationContext(this);
-
-                WebServer webServer = serverFactory.getWebServer(servletContext -> {
-                    servletContext.addServlet("dispatcherServlet", dispatcherServlet)
-                            .addMapping("/*"); // 18. 프론트컨트롤러
-                }); // 웹 서버 생성 // 파라미터(ServletContextInitializer)를 통해 서블릿 등록 // Lambda식으로 servletContext로 사용
-                webServer.start(); // 톰켓 서블릿 컨테이너가 동작
-            }
-        };
-        applicationContext.register(HellobootApplication.class);
-        applicationContext.refresh();
+        SpringApplication.run(HellobootApplication.class, args);
     }
+
+
 
 }
