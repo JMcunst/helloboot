6f55107962f959ac89aea54b4aa1f502694187ff
jmcunst <xnslqjtmghaf@gmail.com>
2023-08-14 22:48:26 +0900

    feat: bean lifecycle

--- src/main/java/jmcunst/spring001/helloboot/HellobootApplication.java
+++ src/main/java/jmcunst/spring001/helloboot/HellobootApplication.java
@@ -4,23 +4,23 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 // 스프링 컨테이너가 팩토리 메소드를 가진 클래스다라는 것을 인지하도록 함
 @Configuration
+@ComponentScan
 public class HellobootApplication {
-    // 28. 팩토리 메소드 사용.
-    @Bean // 28. 스프링 컨테이너에게 알려준다.
-    public HelloController helloController(HelloService helloService) {
-        return new HelloController(helloService);
+    @Bean
+    public ServletWebServerFactory servletWebServerFactory() {
+        return new TomcatServletWebServerFactory();
     }
 
-    // 28. 팩토리 메소드 사용.
-    @Bean // 28. 스프링 컨테이너에게 알려준다.
-    public HelloService helloService() { // 28. 인터페이스 타입으로 리턴하기
-        return new SimpleHelloService();
+    @Bean
+    public DispatcherServlet dispatcherServlet(){
+        return new DispatcherServlet();
     }
 
     public static void main(String[] args) {
@@ -29,10 +29,13 @@ public class HellobootApplication {
             @Override
             protected void onRefresh() {
                 super.onRefresh();
-                ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+                ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
+                DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
+                dispatcherServlet.setApplicationContext(this);
+
                 WebServer webServer = serverFactory.getWebServer(servletContext -> {
-                    servletContext.addServlet("dispatcherServlet", new DispatcherServlet(this)
-                    ).addMapping("/*"); // 18. 프론트컨트롤러
+                    servletContext.addServlet("dispatcherServlet", dispatcherServlet)
+                            .addMapping("/*"); // 18. 프론트컨트롤러
                 }); // 웹 서버 생성 // 파라미터(ServletContextInitializer)를 통해 서블릿 등록 // Lambda식으로 servletContext로 사용
                 webServer.start(); // 톰켓 서블릿 컨테이너가 동작
             }
--- src/main/java/jmcunst/spring001/helloboot/SimpleHelloService.java
+++ src/main/java/jmcunst/spring001/helloboot/SimpleHelloService.java
@@ -1,12 +1,8 @@
 package jmcunst.spring001.helloboot;
 
-import java.util.Objects;
-
 public class SimpleHelloService implements HelloService {
     @Override
     public String sayHello(String name){
-        SimpleHelloService helloService = new SimpleHelloService();
-
-        return helloService.sayHello(Objects.requireNonNull(name));
+        return "Hello " + name;
     }
 }
