6a4e7a5a915ecfcf7d0cafd11351a6c418aa67fd
jmcunst <xnslqjtmghaf@gmail.com>
2023-08-16 20:44:38 +0900

    feat: 3.3 서블릿 등록

--- src/main/java/jmcunst/spring001/helloboot/HellobootApplication2.java
+++ src/main/java/jmcunst/spring001/helloboot/HellobootApplication2.java
@@ -1,14 +1,29 @@
 package jmcunst.spring001.helloboot;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+
+import java.io.IOException;
 
 public class HellobootApplication2 {
     public static void main(String[] args){
         // 3.2. 서블릿 컨테이너 띄우기
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-        WebServer webServer = serverFactory.getWebServer();
+        WebServer webServer = serverFactory.getWebServer(serverContext -> { // 3.3 Servlet 등록
+            serverContext.addServlet("hello", new HttpServlet() {
+                @Override
+                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                    resp.setStatus(200);
+                    resp.setContentType("text/plain");
+                    resp.getWriter().println("Hello Servlet");
+                }
+            });
+        });
         webServer.start();
     }
 }
