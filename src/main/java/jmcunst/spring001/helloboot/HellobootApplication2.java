8b2ebab2566dbf83af768af65258502a5c6294b0
jmcunst <xnslqjtmghaf@gmail.com>
2023-08-16 22:19:22 +0900

    feat: 4.5 DispathcerServlet 전환

--- src/main/java/jmcunst/spring001/helloboot/HellobootApplication2.java
+++ src/main/java/jmcunst/spring001/helloboot/HellobootApplication2.java
@@ -1,23 +1,15 @@
 package jmcunst.spring001.helloboot;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-
-import java.io.IOException;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 public class HellobootApplication2 {
     public static void main(String[] args){
         // 4.1 스프링 컨테이너 사용
-        GenericApplicationContext applicationContext = new GenericApplicationContext();
+        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
         applicationContext.registerBean(HellobootApplication2.class);
         applicationContext.registerBean(SimpleHelloService2.class); // 4.4 의존 오브젝트 DI 적용
         applicationContext.refresh();
@@ -25,23 +17,8 @@ public class HellobootApplication2 {
         // 3.2. 서블릿 컨테이너 띄우기
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
         WebServer webServer = serverFactory.getWebServer(serverContext -> { // 3.3 Servlet 등록
-            serverContext.addServlet("frontcontroller", new HttpServlet() { // 3.6 프론트 컨트롤러
-                @Override
-                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-                    if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())){
-                        String name = req.getParameter("name");
-
-                        HelloController2 helloController2 = applicationContext.getBean(HelloController2.class);
-                        String ret = helloController2.hello(name);
-
-                        resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
-                        resp.getWriter().println(ret);
-                    }else{
-                        resp.setStatus(HttpStatus.NOT_FOUND.value());
-                    }
-
-                }
-            }).addMapping("/hello");
+            serverContext.addServlet("dispatcherServlet", new DispatcherServlet(applicationContext) // 4.5 DispatcherServlet 전환
+            ).addMapping("/*");
         });
         webServer.start();
     }
