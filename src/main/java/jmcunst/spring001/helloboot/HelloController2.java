139988c21f701b3ba6d7598d71d20c8d2def1a12
jmcunst <xnslqjtmghaf@gmail.com>
2023-08-16 22:26:32 +0900

    feat: 4.7. 스프링 컨테이너가 생성될때 서블릿 컨테이너도 생성되게끔

--- src/main/java/jmcunst/spring001/helloboot/HelloController2.java
+++ src/main/java/jmcunst/spring001/helloboot/HelloController2.java
@@ -1,13 +1,21 @@
 package jmcunst.spring001.helloboot;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import java.util.Objects;
 
+@RequestMapping("/hello")
 public class HelloController2 {
     private final HelloService helloService2;
 
     public HelloController2(HelloService helloService){
         this.helloService2 = helloService;
     }
+
+    @GetMapping
+    @ResponseBody
     public String hello(String name){
         return helloService2.sayHello(Objects.requireNonNull(name));
     }
--- src/main/java/jmcunst/spring001/helloboot/HellobootApplication2.java
+++ src/main/java/jmcunst/spring001/helloboot/HellobootApplication2.java
@@ -9,17 +9,23 @@ import org.springframework.web.servlet.DispatcherServlet;
 public class HellobootApplication2 {
     public static void main(String[] args){
         // 4.1 스프링 컨테이너 사용
-        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
+        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() { // 4.7. 스프링 컨테이너가 생성될때 서블릿 컨테이너도 생성되게끔
+            @Override
+            protected void onRefresh() {
+                super.onRefresh();
+
+                // 3.2. 서블릿 컨테이너 띄우기
+                ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+                WebServer webServer = serverFactory.getWebServer(serverContext -> { // 3.3 Servlet 등록
+                    serverContext.addServlet("dispatcherServlet", new DispatcherServlet(this) // 4.5 DispatcherServlet 전환
+                    ).addMapping("/*");
+                });
+                webServer.start();
+
+            }
+        };
         applicationContext.registerBean(HellobootApplication2.class);
         applicationContext.registerBean(SimpleHelloService2.class); // 4.4 의존 오브젝트 DI 적용
         applicationContext.refresh();
-
-        // 3.2. 서블릿 컨테이너 띄우기
-        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-        WebServer webServer = serverFactory.getWebServer(serverContext -> { // 3.3 Servlet 등록
-            serverContext.addServlet("dispatcherServlet", new DispatcherServlet(applicationContext) // 4.5 DispatcherServlet 전환
-            ).addMapping("/*");
-        });
-        webServer.start();
     }
 }
