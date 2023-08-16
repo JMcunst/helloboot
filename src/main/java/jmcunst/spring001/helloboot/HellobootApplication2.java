ec4aa39b8b5f1d14311ffb3dd9361cb9c0963f89
jmcunst <xnslqjtmghaf@gmail.com>
2023-08-16 21:01:35 +0900

    feat: 3.6 프론트 컨트롤러

--- src/main/java/jmcunst/spring001/helloboot/HellobootApplication2.java
+++ src/main/java/jmcunst/spring001/helloboot/HellobootApplication2.java
@@ -7,6 +7,8 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 
 import java.io.IOException;
 
@@ -15,14 +17,21 @@ public class HellobootApplication2 {
         // 3.2. 서블릿 컨테이너 띄우기
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
         WebServer webServer = serverFactory.getWebServer(serverContext -> { // 3.3 Servlet 등록
-            serverContext.addServlet("hello", new HttpServlet() {
+            serverContext.addServlet("frontcontroller", new HttpServlet() { // 3.6 프론트 컨트롤러
                 @Override
                 protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-                    resp.setStatus(200);
-                    resp.setContentType("text/plain");
-                    resp.getWriter().println("Hello Servlet");
+                    if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())){
+                        String name = req.getParameter("name");
+
+                        resp.setStatus(HttpStatus.OK.value());
+                        resp.setContentType("text/plain");
+                        resp.getWriter().println("Hello " + name);
+                    }else{
+                        resp.setStatus(HttpStatus.NOT_FOUND.value());
+                    }
+
                 }
-            });
+            }).addMapping("/hello");
         });
         webServer.start();
     }
