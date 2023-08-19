0644bee4e00b7f247ba36a7ec8f695aa807621d2
jmcunst <xnslqjtmghaf@gmail.com>
2023-08-19 17:41:44 +0900

    feat: 7.2: Conditional 애노테이션과 Condition

--- src/main/java/jmcunst/spring001/config/autoconfig/JettyWebServerConfig.java
+++ src/main/java/jmcunst/spring001/config/autoconfig/JettyWebServerConfig.java
@@ -4,11 +4,23 @@ import jmcunst.spring001.config.MyAutoConfiguration;
 import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.core.type.AnnotatedTypeMetadata;
 
 @MyAutoConfiguration
+@Conditional(JettyWebServerConfig.JettyCondition.class)
 public class JettyWebServerConfig {
     @Bean("jettyWebServerFactory")
     public ServletWebServerFactory servletWebServerFactory() {
         return new JettyServletWebServerFactory();
+    }
+
+    static class JettyCondition implements Condition { // 7.2 중첩 클래스
+        @Override
+        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+            return true;
+        }
     }
 }
