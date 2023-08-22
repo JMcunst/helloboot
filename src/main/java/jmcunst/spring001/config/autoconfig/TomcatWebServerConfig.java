dd0dd9f1533f41c871bc71cc515ea47a5ee1b8f7
jmcunst <xnslqjtmghaf@gmail.com>
2023-08-22 14:04:59 +0900

    feat: componentscan class bean

--- src/main/java/jmcunst/spring001/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/jmcunst/spring001/config/autoconfig/TomcatWebServerConfig.java
@@ -2,16 +2,18 @@ package jmcunst.spring001.config.autoconfig;
 
 import jmcunst.spring001.config.ConditionalMyOnClass;
 import jmcunst.spring001.config.MyAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
 
 @MyAutoConfiguration
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
     @Bean("tomcatWebServerFactory")
+    @ConditionalOnMissingBean
     public ServletWebServerFactory servletWebServerFactory() {
         return new TomcatServletWebServerFactory();
     }
-
 }
