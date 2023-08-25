2b33e389a13985560173e4f3978e2fefe4b40102
jmcunst <xnslqjtmghaf@gmail.com>
2023-08-25 16:41:54 +0900

    feat: 8.2 자동 구성에 Environment 프로퍼티 적용

--- src/main/java/jmcunst/spring001/helloboot/HellobootApplication.java
+++ src/main/java/jmcunst/spring001/helloboot/HellobootApplication.java
@@ -10,5 +10,4 @@ public class HellobootApplication {
         // 28. GenericWebApplicationContext은 자바 코드로 만든 환경설정 정보를 읽을 수 없다.
         SpringApplication.run(HellobootApplication.class, args);
     }
-
 }
