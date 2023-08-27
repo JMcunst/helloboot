0c6508b3a85056f09fc559cc45268fecc2a457be
jmcunst <xnslqjtmghaf@gmail.com>
2023-08-27 16:39:29 +0900

    8.3 Value 애노테이션과 PropertySourcesPlaceholderConfigurer

---
+++ src/main/java/jmcunst/spring001/config/autoconfig/PropertyPlaceholderConfig.java
@@ -0,0 +1,13 @@
+package jmcunst.spring001.config.autoconfig;
+
+import jmcunst.spring001.config.MyAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@MyAutoConfiguration
+public class PropertyPlaceholderConfig {
+    @Bean
+    PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+}
--- src/main/java/jmcunst/spring001/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/jmcunst/spring001/config/autoconfig/TomcatWebServerConfig.java
@@ -2,6 +2,7 @@ package jmcunst.spring001.config.autoconfig;
 
 import jmcunst.spring001.config.ConditionalMyOnClass;
 import jmcunst.spring001.config.MyAutoConfiguration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
@@ -11,11 +12,13 @@ import org.springframework.core.env.Environment;
 @MyAutoConfiguration
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
+    @Value("${contextPath}")
+    String contextPath;
     @Bean("tomcatWebServerFactory")
     @ConditionalOnMissingBean
-    public ServletWebServerFactory servletWebServerFactory(Environment env) {
+    public ServletWebServerFactory servletWebServerFactory() {
         TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
-        factory.setContextPath(env.getProperty("contextPath"));
+        factory.setContextPath(this.contextPath);
         return factory;
     }
 }
